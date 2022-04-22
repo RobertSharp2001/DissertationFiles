3f7c56295e02065d809bcd5f5937054a03af5850
RobertSharp2001 <97159208+RobertSharp2001@users.noreply.github.com>
2022-04-22 13:19:29 +0100

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import time
 
-start = time.time();
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
@@ -16,36 +14,45 @@ def clean_dataset(df):
     indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
     return df[indices_to_keep].astype(np.float64)
 
-filename = "TRAININGDATAEDITED.csv"
-#url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-#names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
-# Assign column names to the dataset
-names = ['actor_account','login_count','logout_count','login_day_count','play_time','avg_money','ip_count','max_level','login_total_day_x','playtime_per_day','sit_count','exp_get_amout','item_get_count','money_get_count','killed_by_pc','killed_by_npc','teleport_count','reborn_count','question_count','login_total_day_y','sit_count_perday','item_get_count_perday','money_get_count_perday','teleport_count_perday','total_party_time','average_party_time','class']
+def run():
+    start = time.time();
+    filename = "TRAININGDATAEDITED.csv"
+    #url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+    #names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+    # Assign column names to the dataset
+    names = ['actor_account','login_count','logout_count','login_day_count','play_time','avg_money','ip_count','max_level','login_total_day_x','playtime_per_day','sit_count','exp_get_amout','item_get_count','money_get_count','killed_by_pc','killed_by_npc','teleport_count','reborn_count','question_count','login_total_day_y','sit_count_perday','item_get_count_perday','money_get_count_perday','teleport_count_perday','total_party_time','average_party_time','class']
 
-# Read dataset to pandas dataframe
-dataset = pd.read_csv(filename, names=names)
-dataset.drop(dataset.columns[0], axis=1, inplace=True)
-dataset = clean_dataset(dataset)
+    # Read dataset to pandas dataframe
+    dataset = pd.read_csv(filename, names=names)
+    dataset.drop(dataset.columns[0], axis=1, inplace=True)
+    dataset = clean_dataset(dataset)
 
-print(dataset.head())
+    print(dataset.head())
 
-X = dataset[['login_count','logout_count','login_day_count','play_time','avg_money','ip_count','max_level','login_total_day_x','playtime_per_day','sit_count','exp_get_amout','item_get_count','money_get_count','killed_by_pc','killed_by_npc','teleport_count','reborn_count','question_count','login_total_day_y','sit_count_perday','item_get_count_perday','money_get_count_perday','teleport_count_perday','total_party_time','average_party_time',]] 
-y = dataset['class'] 
+    X = dataset[['login_count','logout_count','login_day_count','play_time','avg_money','ip_count','max_level','login_total_day_x','playtime_per_day','sit_count','exp_get_amout','item_get_count','money_get_count','killed_by_pc','killed_by_npc','teleport_count','reborn_count','question_count','login_total_day_y','sit_count_perday','item_get_count_perday','money_get_count_perday','teleport_count_perday','total_party_time','average_party_time',]] 
+    y = dataset['class'] 
 
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-scaler = StandardScaler()
-scaler.fit(x_train)
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    scaler = StandardScaler()
+    scaler.fit(x_train)
 
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
+    x_train = scaler.transform(x_train)
+    x_test = scaler.transform(x_test)
 
-gnb = GaussianNB()
-gnb.fit(x_train, y_train)
-y_pred = gnb.predict(x_test)
+    gnb = GaussianNB()
+    gnb.fit(x_train, y_train)
+    y_pred = gnb.predict(x_test)
 
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
 
-end = time.time()
-print("Time elapsed:")
-print(end - start)
+    end = time.time()
+    print("Time elapsed:")
+    print(end - start)
+
+
+for i in range(10):
+    s = "Test number: "
+    print(s + str(i))
+    print("vvvvvvvvvvvvvvvvv")
+    run()
